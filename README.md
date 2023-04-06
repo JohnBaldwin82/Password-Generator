09ee86dea1906824a8d0a643d6fd6a5881f58724
John Baldwin <johnbaldwin82@gmail.com>
2023-04-06 11:36:44 -0600

    new commit after reworking code for re-submittal

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Password-Generator
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+
+Users are able to generate a password between 8-128 characters. They are able to chose
+between uppercase, lowercase, numerical or special characters to be included or excluded
+from their password. After a password is generated it will reset and generate a new separate 
+password for the next user.
+
+https://github.com/JohnBaldwin82/Password-Generator.git
+
+https://johnbaldwin82.github.io/Password-Generator/
+
+
+Screenshot 2023-04-06 113319.png
+
+
+
--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 // Assignment Code
 // document.getElementById("password").value = newPassword;
 var generateBtn = document.querySelector("#generate");
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var chars = '';
   var passwordLength = 0;
-  var newPassword = "";
+  var newPassword = '';
+  var lowercaseString = 'abcdefghijklmnopqrstuvwxyz'
+  var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  var numeric = '0123456789'
+  var specialCharacters = '!@#$%^&*()'
 
   
-
+//Confirm Chars to include each selection from the user
   function generatePassword() {
-    let passwordLength = prompt("Please enter a password length 8-128 characters");
-    confirm("include lowercase, uppercase, numeric, and/or special values")
+    let passwordLength = prompt('Please enter a password length 8-128 characters');
+    var lowercaseConfirm = confirm('include lowercase')
+    if (lowercaseConfirm) {
+      chars += lowercaseString
+    }
+    var uppercaseConfirm = confirm('uppercase')
+    if (uppercaseConfirm) {
+      chars += uppercase
+    }
+    var numericConfirm = confirm('numeric')
+    if (numericConfirm) {
+      chars += numeric
+    }
+    var specialCharactersConfirm = confirm('specialCharacters')
+    if (specialCharactersConfirm) {
+      chars += specialCharacters
+    }
+    console.log(chars)
     newPassword = "";
     for (var i = 0; i < passwordLength; i++) {
   var randomNumber = Math.floor(Math.random() *chars.length);
@@ -18,13 +38,14 @@ var generateBtn = document.querySelector("#generate");
   return newPassword
   }
 
-// Write password to the #password input
+// Write password to the #password input, empty string at the end to
+//reset the results for the next URLSearchParams.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 console.log(password)
   passwordText.textContent = password;
-  
+  chars = ''
 }
 
 // Add event listener to generate button
